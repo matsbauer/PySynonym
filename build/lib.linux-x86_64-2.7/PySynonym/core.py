80c08526eaa18b6a0204571dc4ad3d8a07c8fb8a
matsbauer <mats.bauer@icloud.com>
2018-01-24 14:02:34 +0000

    V 0.0.3

--- build/lib.linux-x86_64-2.7/PySynonym/core.py
+++ build/lib.linux-x86_64-2.7/PySynonym/core.py
@@ -6,31 +6,37 @@
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 
-        
+
 def synonym(word):
     req = Request('https://www.thesaurus.com/browse/%s'%word, headers={'User-Agent': 'Mozilla/5.0'})
     webpage = urlopen(req).read()
     status = BeautifulSoup(webpage, "html5lib").findAll("div", {"class": "relevancy-list"})[0] #Find the HTML class that contains the synonyms
     
-    synonyms = [] #init return list for synonyms
+    synonyms = []
     
     for tag in status.ul.findAll('li'):
         synonyms.append(tag.select('span.text')[0].get_text())
     
     return synonyms
     
-    
+
 def explain(word):
     req = Request('https://www.dictionary.com/browse/%s'%word, headers={'User-Agent': 'Mozilla/5.0'})
     webpage = urlopen(req).read()
-    status = BeautifulSoup(webpage, "html5lib").findAll("div", {"class": "def-content"})[0] #Find the HTML class that contains the synonyms
+    status = BeautifulSoup(webpage, "html5lib").findAll("div", {"class": "def-content"})[0] #Find the HTML class that contains the used definition
     
     string = status.get_text().lstrip().rstrip()
     definition = " ".join(string.split())
     
     return definition
     
+def help():
+    print("Get a list of synonyms using: ps.synonym('word')\n" + \
+            "Get a word definition using: ps.explain('word')\n" + \
+            "Get a word definition using: ps.explain('word')\n")
+    
     
 if __name__ == "__main__":
+    help()
     d = explain('house')
     print(d)
