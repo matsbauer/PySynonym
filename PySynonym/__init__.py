cb0cba483d9aad39df2958cb89c11fd7d654be42
mats c9 <mats.bauer@icloud.com>
2018-01-24 10:22:28 +0000

    Repo Setup

---
+++ PySynonym/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.4
+# encoding: utf-8
+# creator: matsbauer
+# date created: 24.01.2018
+
+try:
+    from .core import *
+except:
+    from core import *
+
--- PySynonym/core.py
+++ PySynonym/core.py
@@ -1,3 +1,26 @@
-import os
+#!/usr/bin/env python3.4
+# encoding: utf-8
+# creator: matsbauer
+# date created: 24.01.2018
 
-print("Testing git Push")
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+
+
+#class PySynonym(object):
+    
+def parser(word):
+    req = Request('http://www.thesaurus.com/browse/%s'%word, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    status = BeautifulSoup(webpage, "html5lib").findAll("div", {"class": "relevancy-list"})[0] #Find the HTML class that contains the synonyms
+    
+    synonyms = [] #init return list for synonyms
+    
+    for tag in status.ul.findAll('li'):
+        synonyms.append(tag.select('span.text')[0].get_text())
+    return synonyms
+    
+if __name__ == "__main__":
+    d = parser('fun')
+    print(d)
+    
