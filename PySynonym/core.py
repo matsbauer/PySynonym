a644e3307bfc924b8e83d1576b4e1b04c80b997f
matsbauer <mats.bauer@icloud.com>
2018-01-25 07:09:29 +0000

    Added functionality for acronyms

--- PySynonym/core.py
+++ PySynonym/core.py
@@ -12,7 +12,6 @@ else:
     from urllib2 import Request, urlopen
     
 
-
 def synonym(word):
     req = Request('https://www.thesaurus.com/browse/%s'%word, headers={'User-Agent': 'Mozilla/5.0'})
     webpage = urlopen(req).read()
@@ -30,6 +29,23 @@ def synonym(word):
     return synonyms
     
 
+def acronym(word):
+    req = Request('https://www.thesaurus.com/browse/%s'%word, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    status = BeautifulSoup(webpage, "html5lib").findAll("div", {"class": "list-holder"})[0] #Find the HTML class that contains the synonyms
+    
+    acronyms = [] #init of return list
+    
+    if sys.version_info >= (3, 0): #if Python 2.x is used, the result has to be encoded.
+        for tag in status.ul.findAll('li'):
+            acronyms.append(tag.select('span.text')[0].get_text())
+    else:
+        for tag in status.ul.findAll('li'):
+            acronyms.append(tag.select('span.text')[0].get_text().encode("utf-8"))
+    
+    return acronyms
+    
+
 def explain(word):
     req = Request('https://www.dictionary.com/browse/%s'%word, headers={'User-Agent': 'Mozilla/5.0'})
     webpage = urlopen(req).read()
@@ -40,15 +56,14 @@ def explain(word):
     
     return definition.capitalize()
    
-    
+
 def help():
     print("Get a list of synonyms using: ps.synonym('word')\n" + \
-            "Get a word definition using: ps.explain('word')\n" + \
             "Get a word definition using: ps.explain('word')\n")
     
-    
+
 if __name__ == "__main__":
-    testcase = synonym('house')
+    testcase = acronym('fun')
     print(testcase)
     testcase = explain('house')
     print(testcase)
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email = "mats.bauer@icloud.com",
     description = ("PySynonym is a plugin that returns a list of synonyms for the given word. This is very useful when programming bots and digital assistants, to increase their understanding. "),
     license = "BSD",
-    keywords = "python synonym plugin",
+    keywords = "python synonym plugin finder definition acronym",
     url = "https://github.com/matsbauer/PySynonym",
     packages=['PySynonym'],
     #long_description=open('readme-pypi.rst').read(),
